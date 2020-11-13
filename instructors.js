82f69767859fa4dfe1308990ad626c97611fb93a
Thomas Chu <thomasuhc@gmail.com>
2020-11-13 01:59:09 -0500

    Completed Data Manipulation in Javascript
    Data Manipulation in Javascript

--- instructors.js
+++ instructors.js
@@ -1,6 +1,37 @@
 const fs = require("fs");
 const data = require("./data.json")
 
+
+exports.show = function(req, res) {
+
+    const { id } = req.params;
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id;
+    })
+
+    if(!foundInstructor) return res.send("Not found Instructors")
+
+    const instructor = {
+        ...foundInstructor,
+        age: "",
+        services: foundInstructor.services.split(","),
+        created_at: "",
+    }
+
+    return res.render("instructors/show", { instructor });
+}
+
+
+
+
+
+
+
+
+
+
+
 exports.post = function(req, res) {
 
     const keys = Object.keys(req.body)
@@ -11,10 +42,21 @@ exports.post = function(req, res) {
         return res.send("Fill all form")
     }
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
+    let {avatar_url, birth, name, services, gender} = req.body;
 
-    data.instructors.push(req.body)
+    birth = Date.parse(birth);
+    const created_at = Date.now();
+    const id = Number(data.instructors.length + 1);
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if(err) return res.send("File error");
