11c088c5e324d0e1ad2c40d3d0b2bf39c4e5dbdf
Thomas Chu <thomasuhc@gmail.com>
2020-11-12 21:05:45 -0500

    Completed Instructor Registration
    Instructor Registration

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const nunjucks = require("nunjucks");
 const routes = require("./routes")
 const server = express();
 
-
+server.use(express.urlencoded({ extended:true }))
 server.use(express.static("public"));
 server.use(routes);
-
 
 
 server.set("view engine", "njk");
