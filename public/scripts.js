ff06c19cb7c47c1942d66c07728dc1e19588b995
Thomas Chu <thomasuhc@gmail.com>
2020-11-19 00:44:17 -0500

    Completed Pagination members page
    Pagination members page

--- public/scripts.js
+++ public/scripts.js
@@ -9,20 +9,17 @@ for (item of menuItems) {
 }
 
 
-function pagination (selectedPage, totalPages) {
+function paginate (selectedPage, totalPages) {
     
         let pages = [], oldPage;
 
         for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
 
         const firstAndLastPage = currentPage == 1 || currentPage == totalPages
-
         const pageAfterSelectedPage =  currentPage <= selectedPage + 2
         const pageBeforeSelectedPage = currentPage >= selectedPage - 2
 
-        if(currentPage == 1 || pageBeforeSelectedPage && pageAfterSelectedPage) {
-
-            pages.push(currentPage)
+        if(firstAndLastPage || pageBeforeSelectedPage && pageAfterSelectedPage) {
 
                 if(oldPage && currentPage - oldPage > 2) {
                     pages.push("...")
@@ -37,4 +34,43 @@ function pagination (selectedPage, totalPages) {
             }
         }
         return pages
+}
+
+function createPagination (pagination) {
+
+        const filter = pagination.dataset.filter;
+        const page = +pagination.dataset.page;
+        const total = +pagination.dataset.total;
+        const pages = paginate(page, total)
+
+        console.log(pages)
+
+        let elements = ""
+
+        for (let page of pages) {
+            
+            if(String(page).includes("...")) {
+
+                elements += `<span>${page}</span>`
+
+            } else {
+
+                if(filter) {
+                    elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+                } else {
+                    elements += `<a href="?page=${page}">${page}</a>`
+                }   
+            }
+        }
+
+        pagination.innerHTML = elements
+
+}
+
+const pagination = document.querySelector(".pagination")
+
+
+
+if(pagination) {
+    createPagination(pagination)
 }
